9e04a8678e46c85dde6152aa3cb25ac8732cea05
Gustavo Sena <gustavosena98@LIVE.COM>
2019-11-30 10:51:29 -0300

    Consertando teste

--- Trabalho Grafos/Trabalho Grafos.cpp
+++ Trabalho Grafos/Trabalho Grafos.cpp
@@ -10,19 +10,20 @@ int main()
 {
 	Grafo grafo;
 
-	grafo.AdicionarAresta(1 - 1, 2 - 1, 20);
-	grafo.AdicionarAresta(2 - 1, 1 - 1, 2);
-	grafo.AdicionarAresta(1 - 1, 3 - 1, 15);
-	grafo.AdicionarAresta(3 - 1, 2 - 1, 4);
-	grafo.AdicionarAresta(3 - 1, 6 - 1, 10);
-	grafo.AdicionarAresta(2 - 1, 6 - 1, 30);
-	grafo.AdicionarAresta(2 - 1, 5 - 1, 10);
-	grafo.AdicionarAresta(6 - 1, 5 - 1, 10);
-	grafo.AdicionarAresta(5 - 1, 4 - 1, 15);
-	grafo.AdicionarAresta(6 - 1, 4 - 1, 4);
+	grafo.AdicionarAresta(1 - 1, 2 - 1, 20, 20);
+	grafo.AdicionarAresta(2 - 1, 1 - 1, 2, 2);
+	grafo.AdicionarAresta(1 - 1, 3 - 1, 15, 15);
+	grafo.AdicionarAresta(3 - 1, 2 - 1, 4, 4);
+	grafo.AdicionarAresta(3 - 1, 6 - 1, 10, 10);
+	grafo.AdicionarAresta(2 - 1, 6 - 1, 30, 30);
+	grafo.AdicionarAresta(2 - 1, 5 - 1, 10, 10);
+	grafo.AdicionarAresta(6 - 1, 5 - 1, 10, 10);
+	grafo.AdicionarAresta(5 - 1, 4 - 1, 15, 15);
+	grafo.AdicionarAresta(6 - 1, 4 - 1, 4, 4);
 
 	grafo.ExibirListaGrafo();
-	grafo.ExibirMatrizGrafo();
+	grafo.ExibirMatrizGrafoCusto();
+	grafo.ExibirMatrizGrafoTempo();
 
-	cout << "Resultado: " << grafo.menorCaminhoDijkstra(0,4) << endl;
+	cout << "Resultado: " << grafo.menorCaminhoDijkstra(0,4,0) << endl;
 }
